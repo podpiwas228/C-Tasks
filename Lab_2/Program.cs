30983938600b407ead82606147dfd30384a0e96a
IVAN LEMESH <lemesh.ivan@student.ehu.lt>
2025-04-08 12:42:26 +0300

    fix

---
+++ Lab_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // create object laptop
+            Storage laptop = new Storage("Laptop", 1200.50, 10);
+
+            // get product info method
+            Console.WriteLine(laptop.GetProductInfo());
+
+            // add quanity(5)
+            laptop.AddStock(5);
+            Console.WriteLine("After adding stock:");
+            Console.WriteLine(laptop.GetProductInfo());
+
+            //sell  product
+            bool isSold = laptop.Sell(8);
+            Console.WriteLine(isSold ? "Sale successful!" : "Not enough stock to sell.");
+            Console.WriteLine(laptop.GetProductInfo());
+
+            //new price
+            laptop.Price = 1300.75;
+            Console.WriteLine("After changing the price:");
+            Console.WriteLine(laptop.GetProductInfo());
+        }
+        // if we have any problem - exception
+        catch (Exception ex)
+        {  
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
