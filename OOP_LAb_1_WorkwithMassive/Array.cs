191b9cc805fe13749469a55c648d171c17be0427
IVAN LEMESH <lemesh.ivan@student.ehu.lt>
2025-04-15 13:06:52 +0300

    сomments

--- OOP_LAb_1_WorkwithMassive/Array.cs
+++ OOP_LAb_1_WorkwithMassive/Array.cs
@@ -2,13 +2,24 @@
 
 class ArrayClass
 {
+    /// <summary>
+    /// Private field that stores a list of integers (List<int>).
+    /// This field is used to hold the elements in the ArrayClass object.
+    /// </summary>
     private List<int> elements;
 
+    /// <summary>
+    /// Initializes a new instance of the ArrayClass and creates an empty list.
+    /// </summary>
     public ArrayClass()
     {
         elements = new List<int>();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the ArrayClass with the provided list of elements.
+    /// </summary>
+    /// <param name="elements">A list of integers that will be used to initialize the elements field.</param>
     public ArrayClass(List<int> elements)
     {
         this.elements = elements;
@@ -17,11 +28,13 @@ class ArrayClass
     /// <summary>
     /// Gets or sets the full list of elements in the array.
     /// </summary>
+    /// <value>The complete list of elements as a List<int>.</value>
     public List<int> Elements
     {
         get => elements;
         set => elements = value;
     }
+
 
     /// <summary>
     /// Calculates how many negative numbers are at odd indices
